0acb7965926688621fa778595251a35ecc36dc99
jeep025 <jli12@risd.edu>
2018-02-05 15:36:40 -0500

    finally
    finally

---
+++ final-copy/final.js
@@ -0,0 +1,125 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "25%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0%";
+}
+
+
+
+$(document).ready(function(){
+    $(".ti").click(function(){
+        $(".textdl").fadeToggle("slow");
+
+    });
+});
+
+
+/*$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});*/
+
+
+$(document).ready(function(){
+    $(".st").click(function(){
+        $(".block").fadeToggle("slow");
+
+    });
+});
+
+
+$(document).ready(function(){
+    $(".pr").click(function(){
+        $(".textpr").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".stt").click(function(){
+        $(".block2").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".bo").click(function(){
+        $(".textbo").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".sttt").click(function(){
+        $(".block3").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".ne").click(function(){
+        $(".textne").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".stttt").click(function(){
+        $(".block4").fadeToggle("slow");
+
+    });
+});
+
+
+$(document).ready(function(){
+    $(".la1").click(function(){
+        $(".textla1").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".la2").click(function(){
+        $(".textla2").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".sttttt").click(function(){
+        $(".block5").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".np").click(function(){
+        $(".textnp").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".stttttt").click(function(){
+        $(".block6").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".kw").click(function(){
+        $(".textkw").fadeToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $(".kwk").click(function(){
+        $(".textkwk").fadeToggle("slow");
+
+    });
+});
